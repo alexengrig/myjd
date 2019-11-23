aaea4495b672925f3aa687f333dd5de7dc6bd06f
Grig Alex <alexengrigdev@gmail.com>
2019-11-23 14:43:19 +0300

    Add Debugger.java and Debuggee.java

---
+++ src/dev/alexengrig/Debuggee.java
@@ -0,0 +1,8 @@
+package dev.alexengrig;
+
+public class Debuggee {
+    public static void main(String[] args) {
+        String text = "Some text";
+        int age = 30;
+    }
+}
---
+++ src/dev/alexengrig/Debugger.java
@@ -0,0 +1,8 @@
+package dev.alexengrig;
+
+public class Debugger {
+    public static void main(String[] args) {
+        System.out.println("Debugger started.");
+        System.out.println("Debugger finished.");
+    }
+}

6bcc1d64b5091c0876fcf64a940f11315549561d
Grig Alex <alexengrigdev@gmail.com>
2019-11-23 19:07:05 +0300

    Add JDI simple implementation

--- src/dev/alexengrig/Debugger.java
+++ src/dev/alexengrig/Debugger.java
@@ -1,8 +1,108 @@
 package dev.alexengrig;
 
+import com.sun.jdi.*;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.connect.LaunchingConnector;
+import com.sun.jdi.connect.VMStartException;
+import com.sun.jdi.event.*;
+import com.sun.jdi.request.BreakpointRequest;
+import com.sun.jdi.request.ClassPrepareRequest;
+import com.sun.jdi.request.EventRequestManager;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public class Debugger {
+    private Class<Debuggee> debugClass;
+    private int[] breakPointLines;
+
     public static void main(String[] args) {
         System.out.println("Debugger started.");
+        Debugger debugger = new Debugger();
+        Class<Debuggee> debugClass = Debuggee.class;
+        debugger.setDebugClass(debugClass);
+        int[] breakPointLines = {6, 7};
+        debugger.setBreakPointLines(breakPointLines);
+        VirtualMachine vm;
+        try {
+            vm = debugger.connectAndLaunchVM();
+            debugger.enableClassPrepareRequest(vm);
+            EventSet eventSet;
+            while ((eventSet = vm.eventQueue().remove()) != null) {
+                for (Event event : eventSet) {
+                    if (event instanceof ClassPrepareEvent) {
+                        debugger.createBreakpoints(vm, (ClassPrepareEvent) event);
+                    }
+                    if (event instanceof BreakpointEvent) {
+                        debugger.displayVariables((BreakpointEvent) event);
+                    }
+                    vm.resume();
+                }
+            }
+        } catch (VMDisconnectedException e) {
+            System.out.println("Virtual Machine is disconnected.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("Debugger finished.");
+    }
+
+    public void setDebugClass(Class<Debuggee> debugClass) {
+        this.debugClass = debugClass;
+    }
+
+    public void setBreakPointLines(int[] breakPointLines) {
+        this.breakPointLines = breakPointLines;
+    }
+
+    private VirtualMachine connectAndLaunchVM() throws VMStartException, IllegalConnectorArgumentsException, IOException {
+        VirtualMachineManager vmManager = Bootstrap.virtualMachineManager();
+        LaunchingConnector launchingConnector = vmManager.defaultConnector();
+        Map<String, Connector.Argument> arguments = launchingConnector.defaultArguments();
+        Connector.Argument mainArgument = arguments.get("main");
+        String debugClassName = debugClass.getName();
+        mainArgument.setValue(debugClassName);
+        return launchingConnector.launch(arguments);
+    }
+
+    public void enableClassPrepareRequest(VirtualMachine vm) {
+        EventRequestManager eventRequestManager = vm.eventRequestManager();
+        ClassPrepareRequest classPrepareRequest = eventRequestManager.createClassPrepareRequest();
+        String debugClassName = debugClass.getName();
+        classPrepareRequest.addClassFilter(debugClassName);
+        classPrepareRequest.enable();
+    }
+
+    public void createBreakpoints(VirtualMachine vm, ClassPrepareEvent event) throws AbsentInformationException {
+        ClassType classType = (ClassType) event.referenceType();
+        for (int lineNumber : breakPointLines) {
+            List<Location> locations = classType.locationsOfLine(lineNumber);
+            if (!locations.isEmpty()) {
+                Location location = locations.get(0);
+                EventRequestManager eventRequestManager = vm.eventRequestManager();
+                BreakpointRequest breakpointRequest = eventRequestManager.createBreakpointRequest(location);
+                breakpointRequest.enable();
+            }
+        }
+    }
+
+    public void displayVariables(LocatableEvent event) throws IncompatibleThreadStateException, AbsentInformationException {
+        StackFrame stackFrame = event.thread().frame(0);
+        Location stackLocation = stackFrame.location();
+        String stackLocationName = stackLocation.toString();
+        String debugClassName = debugClass.getName();
+        if (stackLocationName.contains(debugClassName)) {
+            List<LocalVariable> visibleVariables = stackFrame.visibleVariables();
+            Map<LocalVariable, Value> variableValues = stackFrame.getValues(visibleVariables);
+            System.out.println("Variables at " + stackLocationName + " > ");
+            for (Map.Entry<LocalVariable, Value> entry : variableValues.entrySet()) {
+                LocalVariable variable = entry.getKey();
+                String variableName = variable.name();
+                Value value = entry.getValue();
+                System.out.println(variableName + " = " + value);
+            }
+        }
     }
 }

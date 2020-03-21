c3ea4fad2feb27ff3e656bd0e24b25f973363945
Grig Alex <alexengrigdev@gmail.com>
2020-03-21 12:06:31 +0300

    Fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 ./build.sh
 cd ../build || exit #TODO: Add to classpath
-java -cp "$JDK_18/lib/tools.jar;." dev/alexengrig/Debugger
+java -cp "$JDK_18/lib/tools.jar;." dev/alexengrig/myjd/Debugger
 cd ../scripts || exit #TODO: Workaround to remove build

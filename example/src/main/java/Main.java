9435a821cc630c148fa816dba921b27855b83c90
Grig Alex <alexengrigdev@gmail.com>
2020-03-21 20:37:49 +0300

    Add example

---
+++ example/src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int three = 3;
+        int four = 4;
+        int fife = 5;
+        int size = 6;
+        int seven = 7;
+        int eight = 8;
+        int nine = 9;
+        int ten = 10;
+    }
+}

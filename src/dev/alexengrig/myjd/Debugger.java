eef883ca759073eca8e0442f908a604b9cbe12cc
Grig Alex <alexengrigdev@gmail.com>
2020-03-21 13:25:50 +0300

    Rename Debuggee to ExampleDebuggee

--- src/dev/alexengrig/myjd/Debugger.java
+++ src/dev/alexengrig/myjd/Debugger.java
@@ -18,13 +18,13 @@ import java.util.logging.Logger;
 public class Debugger {
     private static final Logger log = Logger.getLogger(Debugger.class.getSimpleName());
 
-    private Class<Debuggee> debugClass;
+    private Class<ExampleDebuggee> debugClass;
     private int[] breakPointLines;
 
     public static void main(String[] args) {
         log.info("Debugger started.");
         Debugger debugger = new Debugger();
-        Class<Debuggee> debugClass = Debuggee.class;
+        Class<ExampleDebuggee> debugClass = ExampleDebuggee.class;
         debugger.setDebugClass(debugClass);
         int[] breakPointLines = {6, 7};
         debugger.setBreakPointLines(breakPointLines);
@@ -52,7 +52,7 @@ public class Debugger {
         log.info("Debugger finished.");
     }
 
-    public void setDebugClass(Class<Debuggee> debugClass) {
+    public void setDebugClass(Class<ExampleDebuggee> debugClass) {
         this.debugClass = debugClass;
     }
 
--- src/dev/alexengrig/myjd/ExampleDebuggee.java
+++ src/dev/alexengrig/myjd/ExampleDebuggee.java
@@ -1,6 +1,6 @@
 package dev.alexengrig.myjd;
 
-public class Debuggee {
+public class ExampleDebuggee {
     public static void main(String[] args) {
         String text = "Some text";
         int age = 30;

f3d81c3b6e6a992d5427051677c26d80e7b40e56
Grig Alex <alexengrigdev@gmail.com>
2020-03-21 18:56:56 +0300

    Add DebugRunner

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 ./build.sh
 cd ../build || exit #TODO: Add to classpath
-java -cp "$JDK_18/lib/tools.jar;." dev/alexengrig/myjd/Debugger --className dev.alexengrig.myjd.ExampleDebuggee
+java -cp "$JDK_18/lib/tools.jar;." dev/alexengrig/myjd/DebugRunner --className dev.alexengrig.myjd.ExampleDebuggee
 cd ../scripts || exit #TODO: Workaround to remove build

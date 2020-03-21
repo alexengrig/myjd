da1919c5e45c22060e3aef1853e8c6c7577fddfc
Grig Alex <alexengrigdev@gmail.com>
2020-03-21 18:55:59 +0300

    Refactor ArgsUtil

--- src/dev/alexengrig/myjd/util/ArgsUtil.java
+++ src/dev/alexengrig/myjd/util/ArgsUtil.java
@@ -1,8 +1,13 @@
 package dev.alexengrig.myjd.util;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 public final class ArgsUtil {
+
+    public static final String ARG_PREFIX = "--";
+
     private ArgsUtil() {
     }
 
@@ -14,5 +19,29 @@ public final class ArgsUtil {
             }
         }
         return Optional.empty();
+    }
+
+    public static Map<String, String> toMap(String[] args) {
+        final Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < args.length; i++) {
+            final String arg = args[i++];
+            if (!isArg(arg)) {
+                throw new IllegalArgumentException("No arg: " + arg + ".");
+            } else if (i == args.length) {
+                throw new IllegalArgumentException("No value for arg: " + arg + ".");
+            }
+            final String value = args[i];
+            final String name = getArgName(arg);
+            map.put(name, value);
+        }
+        return map;
+    }
+
+    public static boolean isArg(String arg) {
+        return arg.startsWith(ARG_PREFIX);
+    }
+
+    public static String getArgName(String arg) {
+        return arg.substring(ARG_PREFIX.length());
     }
 }

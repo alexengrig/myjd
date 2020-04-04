a0cfaa2215e48b5b3adc999e9ea6f455aa7f2562
Grig Alex <alexengrigdev@gmail.com>
2020-04-04 19:32:57 +0300

    Update ExampleDebuggee.java

--- src/main/java/dev/alexengrig/myjd/ExampleDebuggee.java
+++ src/main/java/dev/alexengrig/myjd/ExampleDebuggee.java
@@ -4,5 +4,10 @@ public class ExampleDebuggee {
     public static void main(String[] args) {
         String text = "Some text";
         int age = 30;
+        method();
+    }
+
+    private static void method() {
+        String string = "This is STRING!!1";
     }
 }

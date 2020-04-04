c0ac3a17e4d65d0439a0d921c85789892eb9b6d0
Grig Alex <alexengrigdev@gmail.com>
2020-04-05 00:00:11 +0300

    Fix className

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 ./build.sh
 cd ../build || exit #TODO: Add to classpath
-"$JDK_18/bin/java" -cp "../lib/tools-1.8.0_241.jar;." dev/alexengrig/myjdi/DebugRunner --className dev.alexengrig.myjd.ExampleDebuggee
+"$JDK_18/bin/java" -cp "../lib/tools-1.8.0_241.jar;." dev/alexengrig/myjdi/DebugRunner --className dev.alexengrig.myjdi.ExampleDebuggee
 cd ../scripts || exit #TODO: Workaround to remove build

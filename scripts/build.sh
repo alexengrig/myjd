481c5774718425dee7cd1c8de6f503f7a539888a
Grig Alex <alexengrigdev@gmail.com>
2020-04-04 19:29:26 +0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,5 @@
 ./clean.sh
 find ../src -name "*.java" > source.txt
-javac -g -d "../build" -cp "$JDK_18/lib/tools.jar;" @source.txt
+mkdir -p ../build
+"$JDK_18/bin/javac" -g -d "../build" -cp "../lib/tools-1.8.0_241.jar;" @source.txt
 rm source.txt

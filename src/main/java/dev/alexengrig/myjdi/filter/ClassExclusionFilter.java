901b4c3ec5b50f23d8696afcdfaa49ba41ee5afd
Grig Alex <alexengrigdev@gmail.com>
2020-04-06 23:36:49 +0300

    Add ClassExclusionFilter

--- src/main/java/dev/alexengrig/myjdi/filter/ClassExclusionFilter.java
+++ src/main/java/dev/alexengrig/myjdi/filter/ClassExclusionFilter.java
@@ -1,25 +1,9 @@
 package dev.alexengrig.myjdi.filter;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 
-public abstract class ClassExclusionFilter {
-    protected final List<String> classExclusionPatterns;
+public interface ClassExclusionFilter {
+    void addClassExclusionFilter(String classPattern);
 
-    public ClassExclusionFilter() {
-        classExclusionPatterns = new ArrayList<>();
-    }
-
-    public List<String> getClassExclusionPatterns() {
-        return classExclusionPatterns;
-    }
-
-    public void addClassExclusionFilter(String classPattern) {
-        classExclusionPatterns.add(classPattern);
-    }
-
-    public void addClassExclusionFilters(Collection<String> classPatterns) {
-        classExclusionPatterns.addAll(classPatterns);
-    }
+    void addClassExclusionFilters(Collection<String> classPatterns);
 }

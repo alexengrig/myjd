171bc59e14e38e7d79c25b8e0453437ce10d648d
Grig Alex <alexengrigdev@gmail.com>
2020-04-06 21:52:29 +0300

    Add EventRequestSetting

---
+++ src/main/java/dev/alexengrig/myjdi/request/EventRequestSetting.java
@@ -0,0 +1,143 @@
+package dev.alexengrig.myjdi.request;
+
+import com.sun.jdi.request.EventRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class EventRequestSetting {
+    protected final Map<Object, Object> properties;
+
+    protected boolean enabled;
+    protected int countFilter;
+    protected int suspendPolicy;
+
+    public EventRequestSetting() {
+        properties = new HashMap<>();
+    }
+
+//    Properties
+
+    public Object addProperty(Object key, Object value) {
+        return properties.put(key, value);
+    }
+
+    public Object getProperty(Object key) {
+        return properties.get(key);
+    }
+
+    public Object removeProperty(Object key) {
+        return properties.remove(key);
+    }
+
+    public boolean removeProperty(Object key, Object value) {
+        return properties.remove(key, value);
+    }
+
+    public boolean containsPropertyKey(Object key) {
+        return properties.containsKey(key);
+    }
+
+    public boolean containsPropertyValue(Object value) {
+        return properties.containsValue(value);
+    }
+
+//    Enable
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean value) {
+        enabled = value;
+    }
+
+    public void enable() {
+        enabled = true;
+    }
+
+    public boolean isDisabled() {
+        return !enabled;
+    }
+
+    public void setDisabled(boolean value) {
+        enabled = !value;
+    }
+
+    public void disable() {
+        enabled = false;
+    }
+
+//    Count filter
+
+    public int getCountFilter() {
+        return countFilter;
+    }
+
+    public void setCountFilter(int value) {
+        countFilter = value;
+    }
+
+//    Suspend policy
+
+    public int getSuspendPolicy() {
+        return suspendPolicy;
+    }
+
+    public void setSuspendPolicy(int value) {
+        suspendPolicy = value;
+    }
+
+    public boolean isSuspendNone() {
+        return EventRequest.SUSPEND_NONE == suspendPolicy;
+    }
+
+    public void setSuspendNone() {
+        suspendPolicy = EventRequest.SUSPEND_NONE;
+    }
+
+    public boolean isSuspendEventThread() {
+        return EventRequest.SUSPEND_EVENT_THREAD == suspendPolicy;
+    }
+
+    public void setSuspendEventThread() {
+        suspendPolicy = EventRequest.SUSPEND_EVENT_THREAD;
+    }
+
+    public boolean isSuspendAll() {
+        return EventRequest.SUSPEND_ALL == suspendPolicy;
+    }
+
+    public void setSuspendAll() {
+        suspendPolicy = EventRequest.SUSPEND_ALL;
+    }
+
+//
+
+    @Override
+    public String toString() {
+        return "EventRequestSetting{" +
+                "properties=" + properties +
+                ", enabled=" + enabled +
+                ", countFilter=" + countFilter +
+                ", suspendPolicy=" + suspendPolicy +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EventRequestSetting that = (EventRequestSetting) o;
+        return enabled == that.enabled &&
+                countFilter == that.countFilter &&
+                suspendPolicy == that.suspendPolicy &&
+                properties.equals(that.properties);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(properties, enabled, countFilter, suspendPolicy);
+    }
+}

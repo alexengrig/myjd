6d004b44a7c191f564b0d7661211b665b1c26b04
Grig Alex <alexengrigdev@gmail.com>
2020-04-08 22:14:46 +0300

    Add file lines

--- src/main/java/dev/alexengrig/myjdi/ui/DebugGUI.java
+++ src/main/java/dev/alexengrig/myjdi/ui/DebugGUI.java
@@ -2,9 +2,16 @@ package dev.alexengrig.myjdi.ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class DebugGUI extends JFrame {
+    private List<String> fileLines;
+    private JPanel fileLinesPane;
+
     public DebugGUI() throws HeadlessException {
+        fileLines = Arrays.asList("First line", "Second line", "Third line", "Fourth line", "Fifth line",
+                "Sixth line", "Seventh line", "Eighth line", "Ninth line", "Tenth line", "Eleventh line");
         setTitle("My Debugger");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(600, 600);
@@ -28,15 +35,25 @@ public class DebugGUI extends JFrame {
     }
 
     private JPanel createDebugPane() {
-        final JPanel panel = new JPanel();
-        panel.add(new JLabel("Debug pane"));
-        return panel;
+        final JPanel debugPane = new JPanel(new BorderLayout());
+        debugPane.add(new JLabel("Debug pane"));
+        return debugPane;
     }
 
     private JPanel createFilePane() {
-        final JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createLoweredBevelBorder());
-        panel.add(new JLabel("File pane"));
-        return panel;
+        final JPanel rootPane = new JPanel(new BorderLayout());
+        fileLinesPane = new JPanel();
+        fileLinesPane.setLayout(new BoxLayout(fileLinesPane, BoxLayout.Y_AXIS));
+        fileLinesPane.setBorder(BorderFactory.createLoweredBevelBorder());
+        for (int i = 0, size = fileLines.size(); i < size; i++) {
+            String fileLine = fileLines.get(i);
+            final String text = String.format("%2d | %s", i + 1, fileLine);
+            final JLabel label = new JLabel(text);
+            label.setBorder(BorderFactory.createLineBorder(Color.BLUE));
+            fileLinesPane.add(label);
+        }
+        rootPane.add(new JScrollPane(fileLinesPane));
+        return rootPane;
     }
+
 }

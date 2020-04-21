e64968f62e28581463b5602e9f4b09eacbf69f98
Grig Alex <alexengrigdev@gmail.com>
2020-04-21 21:52:58 +0300

    Add Shared Memory Attachers

--- src/main/java/dev/alexengrig/myjdi/Attachers.java
+++ src/main/java/dev/alexengrig/myjdi/Attachers.java
@@ -7,6 +7,11 @@ import com.sun.jdi.connect.Connector;
 import java.util.Map;
 
 public final class Attachers {
+    public static final String HOSTNAME = "hostname";
+    public static final String PORT = "port";
+    public static final String TIMEOUT = "timeout";
+    public static final String NAME = "name";
+
     private Attachers() {
     }
 
@@ -65,6 +70,61 @@ public final class Attachers {
         };
     }
 
+//    Shared Memory
+
+    public static Attacher sharedMemoryAttacher() {
+        return () -> {
+            AttachingConnector connector = Connectors.sharedMemoryAttachingConnector();
+            return connector.attach(connector.defaultArguments());
+        };
+    }
+
+    public static Attacher sharedMemoryAttacher(VirtualMachineManager vmManager) {
+        return () -> {
+            AttachingConnector connector = Connectors.sharedMemoryAttachingConnector(vmManager);
+            return connector.attach(connector.defaultArguments());
+        };
+    }
+
+    public static Attacher sharedMemoryAttacher(Map<String, Connector.Argument> arguments) {
+        return () -> {
+            AttachingConnector connector = Connectors.sharedMemoryAttachingConnector();
+            Map<String, Connector.Argument> args = connector.defaultArguments();
+            args.putAll(arguments);
+            return connector.attach(args);
+        };
+    }
+
+    public static Attacher sharedMemoryAttacher(VirtualMachineManager vmManager,
+                                                Map<String, Connector.Argument> arguments) {
+        return () -> {
+            AttachingConnector connector = Connectors.sharedMemoryAttachingConnector(vmManager);
+            Map<String, Connector.Argument> args = connector.defaultArguments();
+            args.putAll(arguments);
+            return connector.attach(args);
+        };
+    }
+
+    public static SharedMemoryAttacherBuilder sharedMemoryAttacherBuilder() {
+        AttachingConnector connector = Connectors.sharedMemoryAttachingConnector();
+        return new SharedMemoryAttacherBuilder(connector.defaultArguments()) {
+            @Override
+            protected Attacher doBuild(Map<String, Connector.Argument> arguments) {
+                return () -> connector.attach(arguments);
+            }
+        };
+    }
+
+    public static SharedMemoryAttacherBuilder sharedMemoryAttacherBuilder(VirtualMachineManager vmManager) {
+        AttachingConnector connector = Connectors.sharedMemoryAttachingConnector(vmManager);
+        return new SharedMemoryAttacherBuilder(connector.defaultArguments()) {
+            @Override
+            protected Attacher doBuild(Map<String, Connector.Argument> arguments) {
+                return () -> connector.attach(arguments);
+            }
+        };
+    }
+
 //    Builder
 
     public static abstract class BaseAttacherBuilder<B extends BaseAttacherBuilder<B>> {
@@ -99,11 +159,11 @@ public final class Attachers {
         }
 
         public SocketAttacherBuilder hostname(String value) {
-            return argument("hostname", value);
+            return argument(HOSTNAME, value);
         }
 
         public SocketAttacherBuilder port(String value) {
-            return argument("port", value);
+            return argument(PORT, value);
         }
 
         public SocketAttacherBuilder port(int value) {
@@ -111,10 +171,33 @@ public final class Attachers {
         }
 
         public SocketAttacherBuilder timeout(String value) {
-            return argument("timeout", value);
+            return argument(TIMEOUT, value);
         }
 
         public SocketAttacherBuilder timeout(long value) {
+            return timeout(Long.toString(value));
+        }
+    }
+
+    public static abstract class SharedMemoryAttacherBuilder extends BaseAttacherBuilder<SharedMemoryAttacherBuilder> {
+        protected SharedMemoryAttacherBuilder(Map<String, Connector.Argument> arguments) {
+            super(arguments);
+        }
+
+        @Override
+        protected SharedMemoryAttacherBuilder self() {
+            return this;
+        }
+
+        public SharedMemoryAttacherBuilder name(String value) {
+            return argument(NAME, value);
+        }
+
+        public SharedMemoryAttacherBuilder timeout(String value) {
+            return argument(TIMEOUT, value);
+        }
+
+        public SharedMemoryAttacherBuilder timeout(long value) {
             return timeout(Long.toString(value));
         }
     }

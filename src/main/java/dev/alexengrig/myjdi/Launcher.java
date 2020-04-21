f242411c243dfd978c62d962466c0367ed3cb6b6
Grig Alex <alexengrigdev@gmail.com>
2020-04-21 19:57:55 +0300

    Add Launcher

---
+++ src/main/java/dev/alexengrig/myjdi/Launcher.java
@@ -0,0 +1,12 @@
+package dev.alexengrig.myjdi;
+
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.connect.VMStartException;
+
+import java.io.IOException;
+
+@FunctionalInterface
+public interface Launcher {
+    VirtualMachine launch() throws IOException, IllegalConnectorArgumentsException, VMStartException;
+}

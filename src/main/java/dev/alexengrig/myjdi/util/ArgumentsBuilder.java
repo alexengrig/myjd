57119b2a733cf9b108c404e8335ddf007b2c2de8
Grig Alex <alexengrigdev@gmail.com>
2020-04-24 22:33:24 +0300

    Add ArgumentsBuilder

---
+++ src/main/java/dev/alexengrig/myjdi/util/ArgumentsBuilder.java
@@ -0,0 +1,25 @@
+package dev.alexengrig.myjdi.util;
+
+import com.sun.jdi.connect.Connector;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class ArgumentsBuilder<B extends ArgumentsBuilder<B>> {
+    protected final Map<String, Connector.Argument> arguments;
+
+    public ArgumentsBuilder(Map<String, Connector.Argument> source) {
+        this.arguments = new HashMap<>(source);
+    }
+
+    public Map<String, Connector.Argument> build() {
+        return arguments;
+    }
+
+    protected B argument(String key, String value) {
+        arguments.get(key).setValue(value);
+        return self();
+    }
+
+    protected abstract B self();
+}

7b9a04750d20363556a5851c261bbdb128164a35
Grig Alex <alexengrigdev@gmail.com>
2020-04-24 19:14:31 +0300

    Add MyConnector

---
+++ src/main/java/dev/alexengrig/myjdi/MyConnector.java
@@ -0,0 +1,12 @@
+package dev.alexengrig.myjdi;
+
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.connect.Connector;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+import com.sun.jdi.connect.VMStartException;
+
+import java.io.IOException;
+
+public interface MyConnector extends Connector {
+    VirtualMachine connect() throws IOException, IllegalConnectorArgumentsException, VMStartException;
+}

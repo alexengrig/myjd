d5631dc2cff24222aaedf436db6d60ba18a106eb
Grig Alex <alexengrigdev@gmail.com>
2020-04-24 20:56:49 +0300

    Add MyAttachConnector

---
+++ src/main/java/dev/alexengrig/myjdi/MyAttachConnector.java
@@ -0,0 +1,30 @@
+package dev.alexengrig.myjdi;
+
+import com.sun.jdi.VirtualMachine;
+import com.sun.jdi.connect.AttachingConnector;
+import com.sun.jdi.connect.IllegalConnectorArgumentsException;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class MyAttachConnector extends ProxyConnector<AttachingConnector> {
+    protected final Attacher attacher;
+
+    public MyAttachConnector(AttachingConnector connector) {
+        this(connector, Attachers.attacher(connector));
+    }
+
+    public MyAttachConnector(AttachingConnector connector, Map<String, Argument> arguments) {
+        this(connector, Attachers.attacher(connector, arguments));
+    }
+
+    protected MyAttachConnector(AttachingConnector connector, Attacher attacher) {
+        super(connector);
+        this.attacher = attacher;
+    }
+
+    @Override
+    public VirtualMachine connect() throws IOException, IllegalConnectorArgumentsException {
+        return attacher.attach();
+    }
+}

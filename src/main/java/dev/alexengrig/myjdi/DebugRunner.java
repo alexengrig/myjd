7dc93ea98961286cf2da512e3e162f5db7b3976c
Grig Alex <alexengrigdev@gmail.com>
2020-04-25 20:22:19 +0300

    Add EventQueueHandler to DebugRunner

--- src/main/java/dev/alexengrig/myjdi/DebugRunner.java
+++ src/main/java/dev/alexengrig/myjdi/DebugRunner.java
@@ -1,48 +1,26 @@
 package dev.alexengrig.myjdi;
 
-import com.sun.jdi.VMDisconnectedException;
 import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.connect.IllegalConnectorArgumentsException;
 import com.sun.jdi.connect.VMStartException;
-import com.sun.jdi.event.*;
 import com.sun.jdi.request.ClassPrepareRequest;
 import com.sun.jdi.request.EventRequestManager;
-import dev.alexengrig.myjdi.util.MyConnector;
 import dev.alexengrig.myjdi.util.MyConnectors;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 public class DebugRunner {
-    public static void main(String[] args) throws IOException, IllegalConnectorArgumentsException, InterruptedException, VMStartException {
-        System.out.println("Started");
-        MyConnector connector = MyConnectors.socket("localhost", 8000);
-        VirtualMachine vm = connector.connect();
+    private static final Logger log = Logger.getLogger(DebugRunner.class.getName());
+
+    public static void main(String[] args) throws IOException, IllegalConnectorArgumentsException, VMStartException {
+        log.info("Started.");
+        VirtualMachine vm = MyConnectors.socket("localhost", 8000).connect();
         EventRequestManager requestManager = vm.eventRequestManager();
         ClassPrepareRequest classPrepareRequest = requestManager.createClassPrepareRequest();
         classPrepareRequest.addClassFilter("dev.alexengrig.example.Main");
         classPrepareRequest.enable();
-        EventQueue queue = vm.eventQueue();
-        vm.resume();
-        boolean running = true;
-        while (running) {
-            EventSet set = queue.remove();
-            for (Event event : set) {
-                if (event instanceof ClassPrepareEvent) {
-                    System.out.println("Prepare class");
-                } else if (event instanceof VMDeathEvent) {
-                    System.out.println("Death");
-                    running = false;
-                    break;
-                } else if (event instanceof VMDisconnectedException) {
-                    System.out.println("Disconnect");
-                    running = false;
-                    break;
-                } else {
-                    System.out.println(event);
-                }
-            }
-            vm.resume();
-        }
-        System.out.println("Finished");
+        new EventQueueHandler(vm.eventQueue()).run();
+        log.info("Finished.");
     }
 }

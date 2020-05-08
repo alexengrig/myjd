c83044a9333f789b248fe69d7d398d3027f6d355
Grig Alex <alexengrigdev@gmail.com>
2020-05-08 22:07:31 +0300

    Create Demo

---
+++ demo/src/dev/alexengrig/myjdi/demo/Demo.java
@@ -0,0 +1,8 @@
+package dev.alexengrig.myjdi.demo;
+
+public class Demo {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("This is Demo!!1");
+        Thread.sleep(4000L);
+    }
+}

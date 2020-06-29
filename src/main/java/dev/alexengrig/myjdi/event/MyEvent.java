db8d05ebdd173d72c931e636c0a71deee34ae458
Grig Alex <alexengrigdev@gmail.com>
2020-06-29 12:04:50 +0300

    Create MyEvent#accept(MyEventHandler)

--- src/main/java/dev/alexengrig/myjdi/event/MyEvent.java
+++ src/main/java/dev/alexengrig/myjdi/event/MyEvent.java
@@ -1,6 +1,8 @@
 package dev.alexengrig.myjdi.event;
 
 import com.sun.jdi.event.Event;
+import dev.alexengrig.myjdi.handle.MyEventHandler;
 
 public interface MyEvent extends Event {
+    void accept(MyEventHandler handler);
 }

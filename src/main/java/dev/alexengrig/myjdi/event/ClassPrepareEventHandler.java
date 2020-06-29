b663d511d695232f43acd6fd23ef912a643530ff
Grig Alex <alexengrigdev@gmail.com>
2020-06-30 00:34:37 +0300

    Remove old even handlers

--- src/main/java/dev/alexengrig/myjdi/event/ClassPrepareEventHandler.java
+++
@@ -1,9 +0,0 @@
-package dev.alexengrig.myjdi.event;
-
-import com.sun.jdi.event.ClassPrepareEvent;
-
-public abstract class ClassPrepareEventHandler extends BaseEventHandler<ClassPrepareEvent> {
-    protected ClassPrepareEventHandler() {
-        super(ClassPrepareEvent.class);
-    }
-}
--- src/main/java/dev/alexengrig/myjdi/event/VMDeathEventHandler.java
+++
@@ -1,9 +0,0 @@
-package dev.alexengrig.myjdi.event;
-
-import com.sun.jdi.event.VMDeathEvent;
-
-public abstract class VMDeathEventHandler extends BaseEventHandler<VMDeathEvent> {
-    public VMDeathEventHandler() {
-        super(VMDeathEvent.class);
-    }
-}

9390fd469be07f3293d9e1df5a290471bea0ebf3
Grig Alex <alexengrigdev@gmail.com>
2020-07-13 23:10:22 +0300

    Create SimpleEventHandler

---
+++ src/main/java/dev/alexengrig/myjdi/SimpleEventHandler.java
@@ -0,0 +1,37 @@
+package dev.alexengrig.myjdi;
+
+import com.sun.jdi.VMDisconnectedException;
+import com.sun.jdi.VirtualMachine;
+import dev.alexengrig.myjdi.event.MyEvent;
+import dev.alexengrig.myjdi.event.MyEventIterator;
+import dev.alexengrig.myjdi.event.MyEventQueue;
+import dev.alexengrig.myjdi.handle.OmitEventHandler;
+
+public class SimpleEventHandler extends OmitEventHandler implements Runnable {
+    protected final MyEventQueue eventQueue;
+
+    protected boolean running;
+    protected boolean vmDied;
+    protected boolean disconnected;
+
+    public SimpleEventHandler(VirtualMachine virtualMachine) {
+        this.eventQueue = MyEventQueue.delegate(virtualMachine.eventQueue());
+    }
+
+    @Override
+    public void run() {
+        running = true;
+        while (running) {
+            try {
+                MyEventIterator iterator = eventQueue.remove().eventIterator();
+                while (iterator.hasNext()) {
+                    MyEvent event = iterator.nextEvent();
+                    event.accept(this);
+                }
+                eventQueue.virtualMachine().resume();
+            } catch (InterruptedException | VMDisconnectedException ignore) {
+                break;
+            }
+        }
+    }
+}

f44024dda1ab27320348157c39ad70a9eb5dfa50
Grig Alex <alexengrigdev@gmail.com>
2020-07-28 22:03:47 +0300

    Add sample tabs

--- src/main/java/dev/alexengrig/myjdi/MyDebuggerGUI.java
+++ src/main/java/dev/alexengrig/myjdi/MyDebuggerGUI.java
@@ -1,9 +1,11 @@
 package dev.alexengrig.myjdi;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyDebuggerGUI extends JFrame {
     public MyDebuggerGUI() {
+        init();
         setTitle("MYJDI");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
@@ -13,5 +15,15 @@ public class MyDebuggerGUI extends JFrame {
 
     public static void main(String[] args) {
         new MyDebuggerGUI();
+    }
+
+    private void init() {
+        BorderLayout layout = new BorderLayout();
+        JPanel contentPane = new JPanel(layout);
+        JTabbedPane fileTabPane = new JTabbedPane(SwingConstants.TOP);
+        fileTabPane.addTab("File 1", new JPanel());
+        fileTabPane.addTab("File 2", new JPanel());
+        contentPane.add(fileTabPane);
+        setContentPane(contentPane);
     }
 }
